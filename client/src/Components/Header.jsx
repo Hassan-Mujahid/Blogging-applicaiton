ca8dda247b41fb262e2636e9282a6f024130ba1d
Hassan-Mujahid <hmworkinse@gmail.com>
2025-02-07 15:00:34 +0500

    removed .env

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Navbar, Button, TextInput } from "flowbite-react";
+import { Navbar, Button, TextInput, Dropdown, Avatar } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const path = useLocation();
-
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2 border-gray-200 dark:border-gray-700">
       <Link
@@ -30,24 +31,47 @@ const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2 ">
-        <Button
-          className="w-12 h-10 hidden sm:inline first-letter:"
-          color="gray"
-          pill
-        >
+        <Button className="w-12 h-10 inline first-letter:" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button
-            outline
-            gradientDuoTone="purpleToBlue"
-            className="relative overflow-hidden   
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                className="cursor-pointer"
+                alt="user"
+                rounded
+                img={currentUser.photoUrl}
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button
+              outline
+              gradientDuoTone="purpleToBlue"
+              className="relative overflow-hidden   
              before:absolute before:inset-0 before:bg-gradient-to-r before:from-purple-500 before:to-blue-500 
              before:opacity-0 hover:before:opacity-100 active:before:opacity-100 active:text-white hover:text-white before:transition-opacity before:duration-500 z-0 cursor-pointer"
-          >
-            <span className="z-10">Sign in</span>
-          </Button>
-        </Link>
+            >
+              <span className="z-10">Sign in</span>
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
